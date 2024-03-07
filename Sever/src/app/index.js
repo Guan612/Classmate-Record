bced4b2bb54425da5854e1aadf710105e76e0539
Guan612 <tiantony53@163.com>
2024-03-07 10:57:14 +0800

    导入环境变量

--- Sever/src/app/index.js
+++ Sever/src/app/index.js
@@ -1,8 +1,13 @@
 const Koa  = require('koa');
 const cros = require('@koa/cors');
+const router = require('../router');
+const {bodyParser} = require('koa-body');
+
 const app = new Koa();
 
 app.use(cros())
+
+app.use(router.routes())
 
 module.exports = app;
 
