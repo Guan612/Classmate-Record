4ccc6f8152a14df523baf8c3b50f8e8d3a148f8a
Guan612 <tiantony53@163.com>
2024-03-07 11:21:55 +0800

    添加照片路由

--- Sever/src/router/index.js
+++ Sever/src/router/index.js
@@ -1,8 +1,9 @@
 const Router = require('koa-router');
 const userRouter = require('./user.router');
+const photoRouter = require('./photo.router');
 
 const router = new Router();
 
-router.use('/users', userRouter.routes());
+router.use('/users', userRouter.routes()).use('/photo', photoRouter.routes());
 
 module.exports = router;
