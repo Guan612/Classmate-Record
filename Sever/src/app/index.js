99cbe2402f0f8e68f0d29893e6ff1726dbdd070d
Guan612 <tiantony53@163.com>
2024-03-07 11:22:10 +0800

    添加body解析

--- Sever/src/app/index.js
+++ Sever/src/app/index.js
@@ -1,11 +1,13 @@
 const Koa  = require('koa');
 const cros = require('@koa/cors');
 const router = require('../router');
-const {bodyParser} = require('koa-body');
+const {koaBody} = require('koa-body');
 
 const app = new Koa();
 
 app.use(cros())
+
+app.use(koaBody())
 
 app.use(router.routes())
 
