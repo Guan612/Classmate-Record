343d8e519b18f57571fdba01c140590d5a2d9472
Guan612 <tiantony53@163.com>
2024-03-07 11:21:40 +0800

    用户路由

--- Sever/src/router/user.router.js
+++ Sever/src/router/user.router.js
@@ -1,12 +1,16 @@
 const Router = require('koa-router');
 const userRouter = new Router();
+const {
+    login,
+    register
+} = require('../controller/user.controller');
 
 userRouter.get('/', (ctx) => {
     ctx.body = 'usersModle';
 })
 
-userRouter.get('/login', (ctx)=>{
-    ctx.body = 'login';
-})
+userRouter.post('/register', register)
+
+userRouter.post('/login', login)
 
 module.exports = userRouter;
