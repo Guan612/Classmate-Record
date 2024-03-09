d4e4209ff992572ed544304cef95ef1e68556393
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-09 10:19:16 +0800

    添加前端api

---
+++ Front/src/api/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import qs from 'qs';
+
+const requrl = axios.create({
+    //基本url
+    baseURL:'http://127.0.0.1:8000/',
+    timeout:2000,
+})
+
+//获取照片api
+export const getPhoto = () =>{
+    return requrl.get('/photos/show')
+}
