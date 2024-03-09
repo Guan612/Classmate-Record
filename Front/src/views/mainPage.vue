870b743d628c71cce3cc42c187c854a9e58f5e63
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-09 15:51:33 +0800

    主页面

---
+++ Front/src/views/mainPage.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import headerBar from '@/components/headerBar.vue';
+    import potoCard from '@/components/potoCard.vue';
+</script>
+
+<template>
+    <div>
+        <headerBar></headerBar>
+        <div>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 flex justify-center">
+                <div v-for="card in 10">
+                    <potoCard></potoCard>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
+
