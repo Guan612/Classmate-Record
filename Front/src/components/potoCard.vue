a24af73fe6261cc9aba97f766a3366e24e6c5760
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-09 15:51:22 +0800

    创建卡片组件

--- Front/src/components/potoCard.vue
+++ Front/src/components/potoCard.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { ref } from 'vue';
 
+let cardText = ref("测试文字");
 </script>
 
 <template>
+    <div class="col">
+        <div class="card mb-3" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">{{cardText}}</p>
+            </div>
+        </div>
+    </div>
 
 </template>
 
 <style scoped>
-
+.card{
+    background: linear-gradient(to right, #5BCEFA, #F5A9B8)
+}
 </style>
