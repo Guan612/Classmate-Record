8dd078e3bcb7a79aeb24d47cc9c9bddae6be2026
Guan612 <tiantony53@163.com>
2024-03-12 20:31:14 +0800

    添加错误处理和校验

--- Sever/src/app/index.js
+++ Sever/src/app/index.js
@@ -2,14 +2,18 @@ const Koa  = require('koa');
 const cros = require('@koa/cors');
 const router = require('../router');
 const {koaBody} = require('koa-body');
+const parameter = require('koa-parameter');
+const errHandler = require('./errHandler')
+
 
 const app = new Koa();
 
 app.use(cros())
+app.use(parameter(app));
+app.use(koaBody());
 
-app.use(koaBody())
-
-app.use(router.routes())
+app.use(router.routes());
+app.on('error', errHandler);
 
 module.exports = app;
 
