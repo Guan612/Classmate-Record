1cf20c5f56e794ea0c1464ade1c73e288c61a51c
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-13 11:18:06 +0800

    Update potoCard.vue

--- Front/src/components/potoCard.vue
+++ Front/src/components/potoCard.vue
@@ -5,15 +5,14 @@ let cardText = ref("测试文字");
 </script>
 
 <template>
-    <div class="col">
-        <div class="card mb-3" style="width: 18rem;">
+    <div class="col flex m-3">
+        <div class="card">
             <img src="https://image.tmdb.org/t/p/original/vo3ODtGpSQxI2ajPuSife2dXNyE.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <p class="card-text">{{cardText}}</p>
             </div>
         </div>
     </div>
-
 </template>
 
 <style scoped>
