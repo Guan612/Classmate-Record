bf072f76dae401f0a8b465090d2fa477dc793f31
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-13 15:58:44 +0800

    Update potoCard.vue

--- Front/src/components/potoCard.vue
+++ Front/src/components/potoCard.vue
@@ -6,14 +6,12 @@ let cardText = ref("测试文字");
 </script>
 
 <template>
-    <template>
-        <NCard title="带封面的卡片">
-            <template #cover>
-            <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
-            </template>
+    <n-card title="带封面的卡片" class="card">
+        <template #cover>
+            <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/6QRJcTEQWRQIa43cgcS9DXXa60W.jpg">
+        </template>
         卡片内容
-        </NCard>
-</template>
+    </n-card>
 </template>
 
 <style scoped>
