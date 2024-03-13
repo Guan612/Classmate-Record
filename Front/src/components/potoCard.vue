352c82487dc7ef223a09b815d2612ef7ab08ad80
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-13 17:02:05 +0800

    重构卡片

--- Front/src/components/potoCard.vue
+++ Front/src/components/potoCard.vue
@@ -3,14 +3,19 @@ import { ref } from 'vue';
 import { NCard } from 'naive-ui';
 
 let cardText = ref("测试文字");
+let title = ref("测试标题");
+let imgSrc = ref("https://media.themoviedb.org/t/p/w220_and_h330_face/6QRJcTEQWRQIa43cgcS9DXXa60W.jpg");
+
 </script>
 
 <template>
-    <n-card title="带封面的卡片" class="card">
+    <n-card :title class="card hover:drop-shadow-2xl hover:scale-110 transform transition-transform duration-300 rounded-lg">
         <template #cover>
-            <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/6QRJcTEQWRQIa43cgcS9DXXa60W.jpg">
+            <img :src="imgSrc">
         </template>
-        卡片内容
+        <div class=" md:text-sm xl:text-base">
+            {{cardText}}
+        </div>
     </n-card>
 </template>
 
