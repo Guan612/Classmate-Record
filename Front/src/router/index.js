b49c6366c695fbe567c63f0350f0f7909d4264d3
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-13 10:05:01 +0800

    Update index.js

--- Front/src/router/index.js
+++ Front/src/router/index.js
@@ -3,6 +3,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/mainPage.vue'),
+      meta:{title:'首页'}
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/loginPage.vue'),
+    },
     
   ]
 })
