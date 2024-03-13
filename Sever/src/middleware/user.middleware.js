46e95cf7e2a27e53728c8c429dfb8a7898b6d7db
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-13 09:35:45 +0800

    添加加密中间件

--- Sever/src/middleware/user.middleware.js
+++ Sever/src/middleware/user.middleware.js
@@ -1,3 +1,5 @@
+const bcry = require('bcryptjs');
+
 const { userFormateError,userAlreadyExited,userRegisterError } = require("../constant/err.type");
 const {getUerInfo} = require("../service/user.service");
 
@@ -15,47 +17,33 @@ const userValidator = async (ctx, next) => {
     await next();
 };
 
-// const verifyUser = async (ctx, next) => {
-//     const { user_name } = ctx.request.body;
-//     try {
-//         let res = await getUerInfo(user_name);
-//         console.log(res);
-//         if (res){
-//             console.log('用户已存在')
-//             ctx.app.emit('error', userAlreadyExited, ctx);
-//             return;
-//         }
-//     } catch (err) {
-//         console.error('获取用户信息错误', err)
-//         ctx.app.emit('error', userRegisterError, ctx)
-//         return
-//     }
-
-//     await next();
-// }
-
 const verifyUser = async (ctx, next) => {
-    const { user_name } = ctx.request.body
+    const { user_name } = ctx.request.body;
   
-    // if (await getUerInfo({ user_name })) {
-    //   ctx.app.emit('error', userAlreadyExited, ctx)
-    //   return
-    // }
     try {
-      const res = await getUerInfo({ user_name })
+      const res = await getUerInfo({ user_name });
   
       if (res) {
-        console.error('用户名已经存在', { user_name })
-        ctx.app.emit('error', userAlreadyExited, ctx)
-        return
+          console.error('用户名已经存在', { user_name });
+          ctx.app.emit('error', userAlreadyExited, ctx);
+          return
       }
     } catch (err) {
-      console.error('获取用户信息错误', err)
-      ctx.app.emit('error', userRegisterError, ctx)
-      return
+        console.error('获取用户信息错误', err);
+        ctx.app.emit('error', userRegisterError, ctx);
+        return;
     }
   
     await next()
+};
+
+const crpytPassword = async (ctx, next) => {
+    const { password } = ctx.request.body;
+    const salt = bcry.genSaltSync(10);
+    // hash保存的是 密文
+    const hash = bcry.hashSync(password, salt);
+    ctx.request.body.password = hash;
+    await next();
 }
 
-module.exports = { userValidator,verifyUser };
+module.exports = { userValidator,verifyUser,crpytPassword };
