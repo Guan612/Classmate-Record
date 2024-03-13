18ed1e5cb4cb491c10c7f2882396515b7f95d1ea
Guan612 <tiantony53@163.com>
2024-03-13 18:10:54 +0800

    Update mainPage.vue

--- Front/src/views/mainPage.vue
+++ Front/src/views/mainPage.vue
@@ -1,11 +1,13 @@
 <script setup>
     import headerBar from '@/components/headerBar.vue';
     import potoCard from '@/components/potoCard.vue';
+    import sideBar from '@/components/sideBar.vue';
 </script>
 
 <template>
     <div>
         <headerBar></headerBar>
+        <sideBar></sideBar>
         <div class="">
             <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4">
                 <div v-for="card in 10" class="flex flex-row m-2">
