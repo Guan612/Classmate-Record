7a51c94924e3fce27afb7c40490e478b97ccd728
Guan612 <tiantony53@163.com>
2024-03-15 17:18:24 +0800

    Update loginCard.vue

--- Front/src/components/loginCard.vue
+++ Front/src/components/loginCard.vue
@@ -4,17 +4,17 @@
 
 <template>
 	<div class="flex justify-center items-center h-screen">
-		<div class="flex flex-flow">
-			<div>
+		<div class="flex md:flex-grow">
+			<div class="flex">
 				登录
-			</div>
-			<div>
+			</div >
+			<div class="flex">
 				用户名
 			</div>
-			<div>
+			<div class="flex">
 				密码
 			</div>
-			<div>
+			<div class="flex">
 				logo
 			</div>
 		</div>
