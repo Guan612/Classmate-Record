ca2ed81cf6a39ae4a4b7ef3d4b7b91eec57a918d
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-18 11:00:19 +0800

    Update api.js

--- Front/src/api/api.js
+++ Front/src/api/api.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
-import qs from 'qs';
+//import qs from 'qs';
 
 const requrl = axios.create({
     //基本url
-    baseURL:'http://127.0.0.1:8000/',
-    timeout:2000,
+    baseURL: 'http://127.0.0.1:8000/',
+    timeout: 2000,
 })
 //注册api
-export const register = (user_name,password) =>{
-    
+export const register = async (user_name, password) => {
+    let user = await user_name.value;
+    let pwd = await password.value;
+    console.log(user, pwd)
 }
 
 //获取照片api
-export const getPhoto = () =>{
+export const getPhoto = () => {
     return requrl.get('/photos/show')
 }
