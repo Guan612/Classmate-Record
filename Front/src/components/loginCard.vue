3602140c4ac48e95a4941fce51b556f85df11545
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-18 10:21:21 +0800

    完善登录卡片

--- Front/src/components/loginCard.vue
+++ Front/src/components/loginCard.vue
@@ -1,35 +1,50 @@
 <script setup>
-import { NInput } from 'naive-ui';
+import { NInput, NButton } from 'naive-ui';
+import { ref } from 'vue';
+let imgSrc = ref('https://upload.wikimedia.org/wikipedia/commons/1/1a/Dolby_logo_2019.svg')
 </script>
 
 <template>
-	<div class="flex justify-center justify-items-center">
-		<div class="shadow-2xl flex-col">
-			<div class="flex flex-row justify-center">
-				<div class="flex">
-					logo
+	<div class="flex justify-center justify-items-center items-center h-screen">
+		<div class="shadow-2xl flex-col card size-200 rounded-lg">
+			<div class="flex flex-row justify-center m-3 md:m-10">
+				<div class="flex mr-2 md:mr-5 w-20 md:w-23 flex-col justify-center">
+					<p class="flex justify-center">登录</p>
+					<img :src="imgSrc">
 				</div>
 				<div class="flex flex-col">
-					<div>
+					<div class="mb-2 md:mb-5">
 						用户
 					</div>
-					<div>
-						用户输入框
+					<div class="mb-2 md:mb-5">
+						<n-input placeholder="请输入用户名" />
 					</div>
-					<div>
+					<div class="mb-2 md:mb-5">
 						密码
 					</div>
-					<div>
-						密码输入框
+					<div class="mb-2 md:mb-5">
+						<n-input placeholder="请输入密码" type="password" />
 					</div>
 				</div>
 			</div>
-			<div>
-				登录按钮与注册按钮
+			<div class="flex flex-col justify-center">
+				<div class="flex justify-center mb-3">
+					<n-button type="info" class="text-black">登录</n-button>
+				</div>
+				<div class="flex justify-center">
+					<router-link to="/register">
+						<n-button strong secondary class="w-100" @click="">还没有注册？</n-button>
+					</router-link>
+				</div>
 			</div>
 		</div>
 	</div>
 
 </template>
 
-<style></style>
+<style>
+.card {
+	background: linear-gradient(to right, #5BCEFA, #F5A9B8)
+	
+}
+</style>
