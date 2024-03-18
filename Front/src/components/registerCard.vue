2bd20d681d224b03696f1a99c1af96da11a29dad
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-18 10:21:37 +0800

    新增注册页面

---
+++ Front/src/components/registerCard.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { NInput, NButton } from 'naive-ui';
+import { ref } from 'vue';
+let imgSrc = ref('https://upload.wikimedia.org/wikipedia/commons/1/1a/Dolby_logo_2019.svg');
+let user_name = ref('');
+let password = ref('');
+let register = () =>{
+    console.log(user_name.value);
+    console.log(password.value);
+}
+
+</script>
+
+<template>
+    <div class="flex justify-center justify-items-center items-center h-screen">
+        <div class="shadow-2xl flex-col card size-200 rounded-lg">
+            <div class="flex flex-row justify-center m-3 md:m-10">
+                <div class="flex mr-2 md:mr-5 w-20 md:w-23 flex-col justify-center">
+                    <p class="flex justify-center">注册</p>
+                    <img :src="imgSrc">
+                </div>
+                <div class="flex flex-col">
+                    <div class="mb-2 md:mb-5">
+                        用户
+                    </div>
+                    <div class="mb-2 md:mb-5">
+                        <n-input placeholder="请输入用户名" v-model="user_name"/>
+                    </div>
+                    <div class="mb-2 md:mb-5">
+                        密码
+                    </div>
+                    <div class="mb-2 md:mb-5">
+                        <n-input placeholder="请输入密码" type="password" v-model="password"/>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col justify-center">
+                <div class="flex justify-center mb-3">
+                    <n-button type="info" class="text-black" @click="register">注册</n-button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    background: linear-gradient(to right, #5BCEFA, #F5A9B8)
+}
+</style>
---
+++ Front/src/views/registerPage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import registerCard from '@/components/registerCard.vue';
+
+</script>
+
+<template>
+    <registerCard></registerCard>
+</template>
+
+<style scoped></style>
