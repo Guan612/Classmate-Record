ec1b44768046f9c731dfd2be1a94ed77cb17d124
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-18 15:47:18 +0800

    创建登录函数

--- Front/src/components/loginCard.vue
+++ Front/src/components/loginCard.vue
@@ -1,7 +1,16 @@
 <script setup>
 import { NInput, NButton } from 'naive-ui';
 import { ref } from 'vue';
+import {login} from '../api/'
 let imgSrc = ref('https://upload.wikimedia.org/wikipedia/commons/1/1a/Dolby_logo_2019.svg')
+let user_name = ref('');
+let password = ref('');
+
+let handleLogin = () => {
+    login(user_name.value, password.value).then(res => {
+        console.log(res)
+    })
+}
 </script>
 
 <template>
@@ -17,19 +26,19 @@ let imgSrc = ref('https://upload.wikimedia.org/wikipedia/commons/1/1a/Dolby_logo
 						用户
 					</div>
 					<div class="mb-2 md:mb-5">
-						<n-input placeholder="请输入用户名" />
+						<n-input placeholder="请输入用户名" v-model:value="user_name"/>
 					</div>
 					<div class="mb-2 md:mb-5">
 						密码
 					</div>
 					<div class="mb-2 md:mb-5">
-						<n-input placeholder="请输入密码" type="password" />
+						<n-input placeholder="请输入密码" type="password" v-model:value="password"/>
 					</div>
 				</div>
 			</div>
 			<div class="flex flex-col justify-center">
 				<div class="flex justify-center mb-3">
-					<n-button type="info" class="text-black">登录</n-button>
+					<n-button type="info" class="text-black" @click="handleLogin">登录</n-button>
 				</div>
 				<div class="flex justify-center">
 					<router-link to="/register">
@@ -47,4 +56,4 @@ let imgSrc = ref('https://upload.wikimedia.org/wikipedia/commons/1/1a/Dolby_logo
 	background: linear-gradient(to right, #5BCEFA, #F5A9B8)
 	
 }
-</style>
+</style>../api
