06182e4ba7cf2ad415e0ccadffe1c7540b61623d
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-18 15:47:43 +0800

    Update counter.js

--- Front/src/stores/counter.js
+++ Front/src/stores/counter.js
@@ -10,3 +10,8 @@ export const useCounterStore = defineStore('counter', () => {
 
   return { count, doubleCount, increment }
 })
+
+//用户token
+export const useUserStore = defineStore('user', () => {
+  const token = ref('')
+})
