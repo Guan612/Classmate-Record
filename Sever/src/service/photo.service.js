18ca3c8abce2cddca08e70b04b15ac5a9e8ea422
Guan612 <tiantony53@163.com>
2024-03-19 18:53:19 +0800

    新增创建照片卡

--- Sever/src/service/photo.service.js
+++ Sever/src/service/photo.service.js
@@ -1,6 +1,20 @@
 const Photo = require('../models/photo.model');
 class PhotoService{
-    async createPhoto(){
+
+    //创建
+    async createPhotoCard(photo_name,photo_describe,photo_url,user_id){
+        const res = await Photo.create({
+            photo_name,
+            photo_describe,
+            photo_url,
+            user_id
+        });
+
+        return res.dataValues;
+    };
+
+    //获取所有照片
+    async getPhotoCard(){
         
     }
 }
