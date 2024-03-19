e7096a4bbce76ad06653fa4e883fd46f1a7c62ac
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-19 16:36:06 +0800

    Update photo.controller.js

--- Sever/src/controller/photo.controller.js
+++ Sever/src/controller/photo.controller.js
@@ -1,6 +1,5 @@
 const path = require('path');
-const uploadFile = require('../middleware/uploadFile.middleware');
-const uploadFileError = require("../constant/err.type")
+const {uploadFileError,unSupportedFileType} = require("../constant/err.type");
 class PhotoController{
 
     async show(ctx, next){
@@ -9,10 +8,20 @@ class PhotoController{
 
     //照片上传api
     async upload(ctx,next){
-        const file = ctx.request.files.file;
+        //console.log(ctx.request.files)
+        const file = ctx.request.files;
+        const fileTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp']
         if(file){
-            uploadFile(file);
-            return path
+            if(!fileTypes.includes(file.type)){
+                return ctx.app.emit('error',unSupportedFileType,ctx);
+            }
+            ctx.body = {
+                code:0,
+                Message:'图片上传成功',
+                result:{
+                    goods_img:path.basename(file.path)
+                }
+            }
         } else {
             return ctx.app.emit('error', uploadFileError,ctx);
         }
