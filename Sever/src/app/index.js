93e9ffcd7e70e4303d5b3e0624e722eadf79bbd5
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-19 16:35:57 +0800

    开启文件上传功能

--- Sever/src/app/index.js
+++ Sever/src/app/index.js
@@ -1,12 +1,28 @@
+const path = require('path');
 const Koa  = require('koa');
 const cros = require('@koa/cors');
 const router = require('../router');
 const {koaBody} = require('koa-body');
+const KoaStatic = require('koa-static')
 const parameter = require('koa-parameter');
 const errHandler = require('./errHandler')
 
 
 const app = new Koa();
+
+app.use(
+    koaBody({
+        multipart: true,
+        formidable: {
+            maxFileSize: 2000*1024*1024,   // 设置上传文件大小最大限制，默认20M
+            uploadDir: path.join(__dirname, '../uploads'),  // 设置文件上传目录
+            keepExtensions: true    // 保持文件的后缀
+        },
+        parsedMethods: ['POST', 'PUT', 'PATCH', 'DELETE'],
+    })
+)
+
+app.use(KoaStatic(path.join(__dirname, '../upload')));
 
 app.use(cros());
 app.use(parameter(app));
