56a2cebaa1794e44b2a542caa419f5daeb0d9f2b
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-19 11:21:23 +0800

    创建照片model

---
+++ Sever/src/model/photo.model.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize')
+
+const seq = require('../db/seq')
+
+const Photo = seq.define('classmeet_photo', {
+    photo_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment:'照片名字'
+    },
+    photo_shootime:{
+        type: DataTypes.DATE,
+        allowNull:true,
+        comment:'照片拍摄时间'
+    },
+    photo_userid:{
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        comment:'照片上传者id'
+    },
+    photo_describe:{
+        type: DataTypes.TEXT,
+        allowNull:true,
+        comment:'照片故事'
+    },
+    photo_url:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment:'照片链接'
+    }
+
+},{paranoid: true});
+
+//Photo.sync({ force: true })
+
+module.exports = Photo;
