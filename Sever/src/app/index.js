90253d9cfd02a6c89a30f4e799111cab9f657fe6
Guan612 <tiantony53@163.com>
2024-03-20 11:28:21 +0800

    Update index.js

--- Sever/src/app/index.js
+++ Sever/src/app/index.js
@@ -10,23 +10,23 @@ const errHandler = require('./errHandler')
 
 const app = new Koa();
 
-// app.use(
-//     koaBody({
-//         multipart: true,
-//         formidable: {
-//             maxFileSize: 2000*1024*1024,   // 设置上传文件大小最大限制，默认20M
-//             uploadDir: path.join(__dirname, '../uploads'),  // 设置文件上传目录
-//             keepExtensions: true    // 保持文件的后缀
-//         },
-//         parsedMethods: ['POST', 'PUT', 'PATCH', 'DELETE'],
-//     })
-// )
+app.use(
+    koaBody({
+        multipart: true,
+        formidable: {
+            maxFileSize: 2000*1024*1024,   // 设置上传文件大小最大限制，默认20M
+            uploadDir: path.join(__dirname, '../uploads'),  // 设置文件上传目录
+            keepExtensions: true    // 保持文件的后缀
+        },
+        parsedMethods: ['POST', 'PUT', 'PATCH', 'DELETE'],
+    })
+)
 
-// app.use(KoaStatic(path.join(__dirname, '../upload')));
+app.use(KoaStatic(path.join(__dirname, '../upload')));
 
 app.use(cros());
 app.use(parameter(app));
-app.use(koaBody());
+// app.use(koaBody());
 
 app.use(router.routes());
 app.on('error', errHandler);
