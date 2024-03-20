33e440e32eef0337d4e05237482a457f39a7e1f2
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-20 09:23:49 +0800

    Update photo.service.js

--- Sever/src/service/photo.service.js
+++ Sever/src/service/photo.service.js
@@ -1,8 +1,8 @@
 const Photo = require('../model/photo.model');
-class PhotoService{
+class PhotoService {
 
     //创建
-    async createPhotoCard(photo_name,photo_describe,photo_url,user_id){
+    async createPhotoCard(photo_name, photo_describe, photo_url, user_id) {
         const res = await Photo.create({
             photo_name,
             photo_describe,
@@ -14,8 +14,24 @@ class PhotoService{
     };
 
     //获取所有照片
-    async getPhotoCard(pageNum,pageSize){
-           
+    async getAllPhotoCard(pageNum, pageSize) {
+        const offset = (pageNum - 1) * pageSize
+        const { count, rows } = await Cart.findAndCountAll({
+            attributes: ['id', 'number', 'selected'],
+            offset: offset,
+            limit: pageSize * 1,
+            include: {
+                model: Photo,
+                as: 'photo_name',
+                attributes: ['id', 'goods_name', 'goods_price', 'goods_img'],
+            },
+        })
+        return {
+            pageNum,
+            pageSize,
+            total: count,
+            list: rows,
+        }
     }
 }
 
