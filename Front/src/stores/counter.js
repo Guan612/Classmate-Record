da06b2e9b268af5afdd340c1419bcde45b9005a8
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-22 15:32:05 +0800

    Update counter.js

--- Front/src/stores/counter.js
+++ Front/src/stores/counter.js
@@ -1,17 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
-})
-
 //用户token
-export const useUserStore = defineStore('user', () => {
+export const useUserStore = defineStore('userInfo', () => {
   const token = ref('')
 })
