2523147d4f39cd59d095ba94ce3ce4ceeb95e09d
Guan612 <tiantony53@163.com>
2024-03-23 11:48:59 +0800

    Update counter.js

--- Front/src/stores/counter.js
+++ Front/src/stores/counter.js
@@ -5,3 +5,12 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('userInfo', () => {
   const token = ref('')
 })
+
+//照片列表
+export const usePhotoListStore = defineStore('photoList', () => {
+  const photoList = ref([]);
+  const getPhotoList = async () => {
+    const res = await getPhoto();
+    photoList.value = res.data.result.list;
+  }
+})
