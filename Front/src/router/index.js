8cb82e6c9a5a689c03e332ad0f5c4149e1189908
Guan612 <tiantony53@163.com>
2024-03-23 16:34:50 +0800

    添加详情路由

--- Front/src/router/index.js
+++ Front/src/router/index.js
@@ -7,17 +7,24 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('@/views/mainPage.vue'),
-      meta:{title:'首页'}
+      meta:{title:'首页'},
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('@/views/loginPage.vue'),
+      meta:{title:'登录'},
     },
     {
       path:"/register",
       name:"register",
-      component:()=>import('@/views/registerPage.vue')
+      component:()=>import('@/views/registerPage.vue'),
+      meta:{title:'注册'},
+    },
+    {
+      path:"/detail",
+      name:"detail",
+      component:()=>import("@/views/detailPage.vue"),
     }
     
   ]
