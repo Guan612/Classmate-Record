9560391093d5e0fa951558a8cc97da3a2063582e
Guan612 <tiantony53@163.com>
2024-03-23 11:35:30 +0800

    Update photo.js

--- Front/src/api/photo.js
+++ Front/src/api/photo.js
@@ -1,6 +1,15 @@
 import requrl from "./index"
+//测试moke api
+// import axios from 'axios';
+
+// const requrl =  axios.create({
+//     //基本url
+//     baseURL: 'http://127.0.0.1:4523/m1/4124022-0-default/',
+//     timeout: 2000,
+// })
 
 //获取照片api
+
 export const getPhoto = () => {
     return requrl.get('/photo/')
 }
