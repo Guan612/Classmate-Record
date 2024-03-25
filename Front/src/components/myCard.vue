49879b04cef1c19360fe46ef1cdd23d865a46b3e
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-25 09:41:06 +0800

    Create myCard.vue

---
+++ Front/src/components/myCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex justify-center justify-items-center items-center h-screen mx-6">
+        <div class="card flex flex-col">
+            <div class="flex flex-row">
+                <div class="">
+                    头像
+                </div>
+                <div class="">
+                    用户名
+                </div>
+            </div>
+            <div class="">
+                <div class="">
+                    上传管理
+                </div>
+                <div class="">
+                    账号管理
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.card {
+    background: linear-gradient(to right, #5BCEFA, #F5A9B8)
+}
+</style>
