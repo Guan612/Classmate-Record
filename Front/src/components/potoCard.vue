24272f2cab232e9681bf004fd83205010b295d31
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-25 09:55:11 +0800

    Update potoCard.vue

--- Front/src/components/potoCard.vue
+++ Front/src/components/potoCard.vue
@@ -3,28 +3,35 @@ import { onMounted, ref } from 'vue';
 import { NCard, NEllipsis } from 'naive-ui';
 
 let imgSrc = ref("https://media.themoviedb.org/t/p/w220_and_h330_face/6QRJcTEQWRQIa43cgcS9DXXa60W.jpg");
+let cardUrl = ref("/detail");
 
 const props = defineProps({
     cardText: String,
     title: String,
+    //cardUrl: String,
     //imgSrc: String,
 })
 
 </script>
 
 <template>
-    <n-card :title class="card hover:drop-shadow-2xl hover:scale-110 transform transition-transform duration-300 rounded-lg">
+    <n-card :title
+        class="card hover:drop-shadow-2xl hover:scale-110 transform transition-transform duration-300 rounded-lg">
         <template #cover>
             <img :src="imgSrc">
         </template>
         <n-ellipsis class=" md:text-sm xl:text-base overflow-hidden">
-            {{cardText}}
+            {{ cardText }}
         </n-ellipsis>
+        <div>
+            <RouterLink :to="cardUrl">详情</RouterLink>
+        </div>
+       
     </n-card>
 </template>
 
 <style scoped>
-.card{
+.card {
     background: linear-gradient(to right, #5BCEFA, #F5A9B8)
 }
 </style>
