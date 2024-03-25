93581462d35de43fea5c68ea0d578fce1900a974
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-25 08:55:28 +0800

    修改详情卡片布局

--- Front/src/components/detailsCard.vue
+++ Front/src/components/detailsCard.vue
@@ -8,29 +8,29 @@ let photoContent = ref("照片内容");
 </script>
 
 <template>
-    <div class="flex justify-center justify-items-center items-center h-screen">
-        <div class="flex flex-col card shadow-2xl rounded-lg">
+    <div class="flex justify-center justify-items-center items-center h-screen mx-6">
+        <div class="flex flex-col card shadow-2xl rounded-lg p-3">
             <div class="flex flex-row justify-items-center">
-                <div>
+                <div class="flex mx-1">
                     返回键
                 </div>
-                <div class="flex">
+                <div class="flex mx-1">
                     <n-avatar round size="large" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
                 </div>
-                <div class="flex">
+                <div class="flex mx-2">
                     {{ userName }}
                 </div>
             </div>
-            <div class="flex">
-                <n-carousel show-arrow>
+            <div class="flex m-5">
+                <n-carousel show-arrow class="rounded-lg">
                     <img class="carousel-img"
                         src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg">
                     <img class="carousel-img"
-                        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg">
+                        src="https://p3.itc.cn/images01/20230925/43d7f951f84549d69fdaa790d1d5dbeb.png">
                     <img class="carousel-img"
                         src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg">
                     <img class="carousel-img"
-                        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg">
+                        src="https://5b0988e595225.cdn.sohucs.com/images/20190723/6250a03233854cf4a72a04511af66da5.jpeg">
                 </n-carousel>
             </div>
             <div class="flex flex-col">
