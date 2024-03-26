c541f5a9e621dec30bf5aa6fb2ac93e600bcad1e
Guan612 <tiantony53@163.com>
2024-03-26 19:36:56 +0800

    Update index.js

--- Sever/src/app/index.js
+++ Sever/src/app/index.js
@@ -15,14 +15,14 @@ app.use(
         multipart: true,
         formidable: {
             maxFileSize: 2000*1024*1024,   // 设置上传文件大小最大限制，默认20M
-            uploadDir: path.join(__dirname, '../uploads'),  // 设置文件上传目录
+            uploadDir: path.join(__dirname, '../uploads/img'),  // 设置文件上传目录
             keepExtensions: true    // 保持文件的后缀
         },
         parsedMethods: ['POST', 'PUT', 'PATCH', 'DELETE'],
     })
 )
 
-app.use(KoaStatic(path.join(__dirname, '../upload')));
+app.use(KoaStatic(path.join(__dirname, '../uploads')));
 
 app.use(cros());
 app.use(parameter(app));
