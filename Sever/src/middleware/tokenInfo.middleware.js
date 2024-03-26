c09e769f2ae69f8be52a4120ec3fd90fa0b8c700
Guan612 <tiantony53@163.com>
2024-03-26 19:29:59 +0800

    Update tokenInfo.middleware.js

--- Sever/src/middleware/tokenInfo.middleware.js
+++ Sever/src/middleware/tokenInfo.middleware.js
@@ -5,7 +5,7 @@ const tokeninfo = async (ctx,next) =>{
     const { authorization = '' } = ctx.request.header;
     const token = authorization.replace('Bearer ', '');
     const user = jwt.verify(token, JWT_SECRET);
-    ctx.state.user = user;
+    // ctx.state.user = user;
     //console.log(user);
     return user
 }
