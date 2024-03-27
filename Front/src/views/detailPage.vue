a61c65b17f31d2d3c8d4ec7c76d3b88b642eb742
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-27 16:48:31 +0800

    Update detailPage.vue

--- Front/src/views/detailPage.vue
+++ Front/src/views/detailPage.vue
@@ -1,27 +1,31 @@
 <script setup>
 import { onMounted, ref } from "vue";
+import { useRoute } from 'vue-router'
 import detailCard from "@/components/detailsCard.vue"
 import headerCard from "@/components/headerBar.vue"
-import {getPhotoDetail} from "@/api/photo"
+import { getPhotoDetail } from "@/api/photo"
+
 let result = ref({})
 let userName = ref("")
+const route = useRoute();
 
-onMounted(async()=>{
-    const res = await getPhotoDetail(3)
+// 获取路由参数
+const getParamValue = () => {
+    const paramValue = route.params.id;
+    return paramValue;
+};
+
+onMounted(async () => {
+    const res = await getPhotoDetail(getParamValue())
     result = res.data.result
     //userName = res.data.result.classmeet_user.user_name
-    console.log(result)
+    //console.log(result)
 })
 </script>
 
 <template>
     <headerCard></headerCard>
-    <detailCard
-    :cardText="result.photo_describe"
-    :title="result.photo_name"></detailCard>
+    <detailCard :cardText="result.photo_describe" :title="result.photo_name"></detailCard>
 </template>
 
-<style scoped>
-
-</style>
-
+<style scoped></style>
