33da3365f9aafbe6cedd1f2ed799dba44832d258
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-27 11:12:13 +0800

    Update photo.js

--- Front/src/api/photo.js
+++ Front/src/api/photo.js
@@ -1,15 +1,10 @@
 import requrl from "./index"
-//测试moke api
-// import axios from 'axios';
 
-// const requrl =  axios.create({
-//     //基本url
-//     baseURL: 'http://127.0.0.1:4523/m1/4124022-0-default/',
-//     timeout: 2000,
-// })
-
-//获取照片api
-
+//照片列表
 export const getPhoto = () => {
     return requrl.get('/photo/')
+}
+
+export const getPhotoDetail = (id) => {
+    return requrl.get(`/photo/detail/${id}/`)
 }
