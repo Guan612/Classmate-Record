c42d207098ba6db0621416109901a0e178b20f6c
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-27 11:12:30 +0800

    Update detailPage.vue

--- Front/src/views/detailPage.vue
+++ Front/src/views/detailPage.vue
@@ -1,11 +1,24 @@
 <script setup>
-    import detailCard from "@/components/detailsCard.vue"
-    import headerCard from "@/components/headerBar.vue"
+import { onMounted, ref } from "vue";
+import detailCard from "@/components/detailsCard.vue"
+import headerCard from "@/components/headerBar.vue"
+import {getPhotoDetail} from "@/api/photo"
+let result = ref({})
+let userName = ref("")
+
+onMounted(async()=>{
+    const res = await getPhotoDetail(3)
+    result = res.data.result
+    //userName = res.data.result.classmeet_user.user_name
+    console.log(result)
+})
 </script>
 
 <template>
     <headerCard></headerCard>
-    <detailCard></detailCard>
+    <detailCard
+    :cardText="result.photo_describe"
+    :title="result.photo_name"></detailCard>
 </template>
 
 <style scoped>
