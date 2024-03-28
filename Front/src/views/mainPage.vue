f1cff0d5da8c1e747361ad973091bde14000408c
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-28 09:40:39 +0800

    Update mainPage.vue

--- Front/src/views/mainPage.vue
+++ Front/src/views/mainPage.vue
@@ -16,7 +16,7 @@ onMounted(() => getPhotoList());
 <template>
     <div>
         <headerBar></headerBar>
-        <div class="">
+        <div class="flex">
             <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4">
                 <div v-for="card in photoList" :key="card.id" class="flex flex-row m-2">
                     <potoCard 
