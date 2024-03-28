e9ca5f3e67902efd0de6cb94b39c3ef98d3098ff
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-28 10:20:40 +0800

    Update uploadCard.vue

--- Front/src/components/uploadCard.vue
+++ Front/src/components/uploadCard.vue
@@ -1,20 +1,33 @@
 <script setup>
-import { NInput, NButton, NUpload } from 'naive-ui';
+import { uploadPhotoFile,uploadPhotoDes } from '@/api/photo';
+import { NInput, NButton, NUpload, unstableCollapseTransitionRtl } from 'naive-ui';
 import { ref } from 'vue';
 
 //必须先上传图片返回名字后再进行其他操作
-const flag = ref(true);
+const flag = ref(false);
+
 const photoName = ref('');
 const photoDescribe = ref('');
 const photoUrl = ref('');
 
+const uploadPhotof = async (file) => {
+    const res = await uploadPhotoFile(file)
+    console.log(res)
+    if(res){
+        photoUrl =  res.result.photo_name
+    }
+}
+
+const uploadPhotoDescribe = async (file) => {
+    const res = await uploadPhotoDes(file)
+}
 </script>
 
 <template>
     <div class="flex justify-center justify-items-center items-center h-screen">
         <div class="card shadow-2xl flex-col card size-300 rounded-lg">
             <div class="flex">
-                <n-upload multiple directory-dnd action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f" :max="5">
+                <n-upload multiple directory-dnd action="http://127.0.0.1:8000//photo/file" :max="5">
                     <n-upload-dragger>
                         <div style="margin-bottom: 12px">
                             <p size="48" :depth="3">
@@ -37,9 +50,9 @@ const photoUrl = ref('');
                 <n-input v-model:value="photoDescribe" type="textarea" placeholder="请输入描述喵" />
             </div>
             <div class="flex">
-                <n-button type="primary" size="large" class="" @click="">上传喵~</n-button>
+                <n-button type="primary" size="large" class="" @click="uploadPhotof">上传喵~</n-button>
                 <n-button type="warning" size="large" class="">保存喵~</n-button>
-                <n-button type="success" size="large" class="" @click="">发布喵~</n-button>
+                <n-button type="success" size="large" class="" @click="uploadPhotoDescribe" v-bind:disabled="flag">发布喵~</n-button>
                 <n-button type="error" size="large">取消喵~</n-button>
             </div>
         </div>
