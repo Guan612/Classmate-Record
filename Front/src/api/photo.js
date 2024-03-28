737a58cf2e1703891ba89c15e717b4ae0ff2b859
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-03-28 09:24:50 +0800

    Update photo.js

--- Front/src/api/photo.js
+++ Front/src/api/photo.js
@@ -5,6 +5,26 @@ export const getPhoto = () => {
     return requrl.get('/photo/')
 }
 
+//照片详情
 export const getPhotoDetail = (id) => {
     return requrl.get(`/photo/detail/${id}/`)
+}
+
+//上传照片文件
+export const uploadPhotoFile = (file) => {
+    return requrl.post('/photo/file', file)
+}
+
+//上传照片描述api
+export const uploadPhoto = async (photo_name, photo_describe, photo_url) => {
+    try {
+        const res = await requrl.post('/photo/', {
+            "photo_name": photo_name,
+            "photo_describe": photo_describe,
+            "photo_url": photo_url
+        });
+        return(res.data.result);
+    } catch (error) {
+        console.error(error);
+    }
 }
