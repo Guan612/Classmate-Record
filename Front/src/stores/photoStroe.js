44fdb37c231c0d05f5329a6dc2d1fc88c38dd20b
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-04-08 16:11:46 +0800

    创建pinia

--- Front/src/stores/photoStroe.js
+++ Front/src/stores/photoStroe.js
@@ -1,11 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-//用户token
-export const useUserStore = defineStore('userInfo', () => {
-  const token = ref('')
-})
-
 //照片列表
 export const usePhotoListStore = defineStore('photoList', () => {
   const photoList = ref([]);
